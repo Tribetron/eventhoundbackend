370c5aa45d9ea98796870f33fed5c0673ca3a70b
Tatenda Chakwesha <programmertatenda@gmail.com>
2021-11-29 17:05:00 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const _port = 1936;
+const mysql = require('mysql');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { create_user } = require('./routes/index');
+const houndRoutes = express.Router();
+const version = 'v1';
+require('./routes/index');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/", houndRoutes);
+// require("env").config();
+
+const db = mysql.createConnection({
+    user:'tatenda',
+    host:'127.0.0.1',
+    password:'Tatenda2021.',
+    database:'eventhound',
+})
+
+// db.then(()=>{
+//     console.log("connection created successfully")
+// })
+
+
+// default access endpoint
+houndRoutes.route("/").get((req, res) => {
+    res.json("Welcome to the eventhound server, contact admin for access");
+    console.log(version+create_user);
+   });
+
+// API to create a new user
+houndRoutes.route("/v1/users/create").post((req,res)=>{
+    const user_data = req.query;
+    const username = user_data.username;
+    const password = user_data.password;
+    const role = user_data.role;
+
+    db.query('INSERT INTO users (username,password,role) VALUES ("'+username+'","'+password+'","'+role+'")',(err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+ 
+app.listen(_port,()=>{
+    console.log("app running on port " + _port)
+})
+
+
+
+
+
+
+
+
+
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+exports.create_user = '/users/create';
+exports.edit_user = '/users/edit';
+exports.single_user = '/users/single';
+exports.login_user = '/users/login';
+exports.list_users = '/users/list';
+exports.delete_account = '/users/delete';
+exports.forgot_password = '/users/password-forgot';
+exports.google_login = '/users/login/google';
+exports.facebook_login = '/users/login/facebook';
+
+exports.create_venue = '/venue/create';
+exports.edit_venue = '/venue/edit';
+exports.list_venues = '/venue/list';
+exports.list_single_venue = '/venue/single';
+exports.search_venue = '/venue/search';
+// book venue takes parameters, (manual confirmation, auto confirmation, with email notifcations based on confirmation method);
+exports.book_venue = '/venue/book';
+exports.delete_venue = '/venue/delete';
+
+
+exports.process_payment = '/payments/process';
+exports.list_payment = '/payments/single';
+
+exports.help = '/help/search';
+
+
