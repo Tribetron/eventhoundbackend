fa5cd7b2b75a9a94520ce654fa30dfc90788fd85
Kudakwashe Rukuni <52784179+OGJAYYOU@users.noreply.github.com>
2021-12-02 13:29:44 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const { create_user } = require('./routes/index');
 const houndRoutes = express.Router();
 const version = 'v1';
-require('./routes/index');
+require('./routes/index.js');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -15,9 +15,10 @@ app.use("/", houndRoutes);
 // require("env").config();
 
 const db = mysql.createConnection({
-    user:'tatenda',
+    user:'root',
     host:'127.0.0.1',
-    password:'Tatenda2021.',
+    password: '',
+    //password:'Tatenda2021.',
     database:'eventhound',
 })
 
@@ -25,37 +26,120 @@ const db = mysql.createConnection({
 //     console.log("connection created successfully")
 // })
 
-
-// default access endpoint
+// 0.default access endpoint
 houndRoutes.route("/").get((req, res) => {
-    res.json("Welcome to the eventhound server, contact admin for access");
+    res.json("Welcome to the eventhound server, contact admin for access");   // /v1/users/create
     console.log(version+create_user);
-   });
+});
 
-// API to create a new user
-houndRoutes.route("/v1/users/create").post((req,res)=>{
+// 1.API to create a new user
+//houndRoutes.route("/v1/users/create").post((req,res)=>{
+houndRoutes.route(create_user).post((req,res)=>{
     const user_data = req.query;
     const username = user_data.username;
     const password = user_data.password;
     const role = user_data.role;
 
-    db.query('INSERT INTO users (username,password,role) VALUES ("'+username+'","'+password+'","'+role+'")',(err,result)=>{
+    db.query('INSERT INTO users (username, password, role) VALUES ("'+username+'", "'+password+'", "'+role+'")', (err,result)=>{
         if(err){
         res.json(err);
         }
         res.send(result);
     })
 })
+
+// 2.API to login user
+houndRoutes.route("/v1/users/login").get((req,res)=>{
+    const user_data = req.query;
+    const username = user_data.username;
+    const password = user_data.password;
+    const role = user_data.role;
+    db.query("SELECT * FROM users WHERE username = '"+username+"' && password = '"+password+"';", (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+// 3.API to edit user
+houndRoutes.route("/v1/users/edit").post((req,res)=>{
+    const user_data = req.query;
+    const username = user_data.username;
+    const password = user_data.password;
+    const role = user_data.role;
+    db.query("UPDATE users SET username = '"+username+"', password = '"+password+"', role = '"+role+"'", (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+// 4.API to view a single new user
+houndRoutes.route("/v1/users/single").get((req,res)=>{
+    const user_data = req.query;
+    const username = user_data.username;
+    const password = user_data.password;
+    const role = user_data.role;
+
+    db.query("SELECT * FROM users WHERE username = '"+username+"' && password = '"+password+"';", (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+// 5.API to list all user
+houndRoutes.route("/v1/users/list").get((req,res)=>{
+    const user_data = req.query;
+    const username = user_data.username;
+    const password = user_data.password;
+    const role = user_data.role;
+
+    db.query('SELECT * FROM users',(err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+// 6.API to delete a user
+houndRoutes.route("/v1/users/delete").post((req,res)=>{
+    const user_data = req.query;
+    const username = user_data.username;
+    const password = user_data.password;
+    const role = user_data.role;
+
+    db.query("DELETE FROM users WHERE username = '"+username+"' && role = '"+role+"';",(err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+// 7.API to reset password
+houndRoutes.route("/v1/users/password-forgot").post((req,res)=>{
+    const user_data = req.query;
+    const username = user_data.username;
+    db.query("UPDATE users SET password = '"+password+"'", (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+// 8.API to google login
+
+
+// 9.API to facebook login
+
  
 app.listen(_port,()=>{
     console.log("app running on port " + _port)
 })
-
-
-
-
-
-
-
-
 
