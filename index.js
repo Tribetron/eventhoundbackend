2f7958cdbe59e617cac7d97fc80a648287a6078e
Kudakwashe Rukuni <52784179+OGJAYYOU@users.noreply.github.com>
2021-12-05 18:44:54 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
-const _port = 1936;
+const _port = 1939;
 const mysql = require('mysql');
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const { create_user } = require('./routes/index');
+
+const { create_user } = require('./routes/index.js');
+const { login_user } = require('./routes/index.js');
+const { edit_user } = require('./routes/index.js');
+const { list_users } = require('./routes/index.js');
+const { delete_account } = require('./routes/index.js');
+const { single_user } = require('./routes/index.js');
+const { search_venue } = require('./routes/index.js');
+const { delete_venue } = require('./routes/index.js');
+const { list_single_venue } = require('./routes/index.js');
+const { book_venue } = require('./routes/index.js');
+
+const { forgot_password } = require('./routes/index.js');
+
 const houndRoutes = express.Router();
 const version = 'v1';
 require('./routes/index.js');
@@ -12,13 +25,13 @@ require('./routes/index.js');
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/", houndRoutes);
+app.use(express.static("public"));
 // require("env").config();
 
 const db = mysql.createConnection({
     user:'root',
     host:'127.0.0.1',
     password: '',
-    //password:'Tatenda2021.',
     database:'eventhound',
 })
 
@@ -26,21 +39,20 @@ const db = mysql.createConnection({
 //     console.log("connection created successfully")
 // })
 
-// 0.default access endpoint
+/*/ 0.default access endpoint
 houndRoutes.route("/").get((req, res) => {
-    res.json("Welcome to the eventhound server, contact admin for access");   // /v1/users/create
-    console.log(version+create_user);
-});
+    res.json("Welcome to the eventhound server, contact admin for access");
+    console.log(create_user);
+});*/
 
 // 1.API to create a new user
-//houndRoutes.route("/v1/users/create").post((req,res)=>{
 houndRoutes.route(create_user).post((req,res)=>{
     const user_data = req.query;
     const username = user_data.username;
     const password = user_data.password;
     const role = user_data.role;
 
-    db.query('INSERT INTO users (username, password, role) VALUES ("'+username+'", "'+password+'", "'+role+'")', (err,result)=>{
+    db.query('INSERT INTO users (username, password, role) VALUES ("'+username+'", "'+password+'", "'+role+'");', (err,result)=>{
         if(err){
         res.json(err);
         }
@@ -49,7 +61,7 @@ houndRoutes.route(create_user).post((req,res)=>{
 })
 
 // 2.API to login user
-houndRoutes.route("/v1/users/login").get((req,res)=>{
+houndRoutes.route(login_user).get((req,res)=>{
     const user_data = req.query;
     const username = user_data.username;
     const password = user_data.password;
@@ -63,12 +75,12 @@ houndRoutes.route("/v1/users/login").get((req,res)=>{
 })
 
 // 3.API to edit user
-houndRoutes.route("/v1/users/edit").post((req,res)=>{
+houndRoutes.route(edit_user).post((req,res)=>{
     const user_data = req.query;
     const username = user_data.username;
     const password = user_data.password;
     const role = user_data.role;
-    db.query("UPDATE users SET username = '"+username+"', password = '"+password+"', role = '"+role+"'", (err,result)=>{
+    db.query("UPDATE users SET username = '"+username+"', password = '"+password+"', role = '"+role+"';", (err,result)=>{
         if(err){
         res.json(err);
         }
@@ -77,7 +89,7 @@ houndRoutes.route("/v1/users/edit").post((req,res)=>{
 })
 
 // 4.API to view a single new user
-houndRoutes.route("/v1/users/single").get((req,res)=>{
+houndRoutes.route(single_user).get((req,res)=>{
     const user_data = req.query;
     const username = user_data.username;
     const password = user_data.password;
@@ -92,13 +104,13 @@ houndRoutes.route("/v1/users/single").get((req,res)=>{
 })
 
 // 5.API to list all user
-houndRoutes.route("/v1/users/list").get((req,res)=>{
+houndRoutes.route(list_users).get((req,res)=>{
     const user_data = req.query;
     const username = user_data.username;
     const password = user_data.password;
     const role = user_data.role;
 
-    db.query('SELECT * FROM users',(err,result)=>{
+    db.query('SELECT * FROM users;',(err,result)=>{
         if(err){
         res.json(err);
         }
@@ -107,7 +119,7 @@ houndRoutes.route("/v1/users/list").get((req,res)=>{
 })
 
 // 6.API to delete a user
-houndRoutes.route("/v1/users/delete").post((req,res)=>{
+houndRoutes.route(delete_account).post((req,res)=>{
     const user_data = req.query;
     const username = user_data.username;
     const password = user_data.password;
@@ -122,10 +134,10 @@ houndRoutes.route("/v1/users/delete").post((req,res)=>{
 })
 
 // 7.API to reset password
-houndRoutes.route("/v1/users/password-forgot").post((req,res)=>{
+houndRoutes.route(forgot_password).post((req,res)=>{
     const user_data = req.query;
     const username = user_data.username;
-    db.query("UPDATE users SET password = '"+password+"'", (err,result)=>{
+    db.query("UPDATE users SET password = '"+password+"';", (err,result)=>{
         if(err){
         res.json(err);
         }
@@ -138,7 +150,83 @@ houndRoutes.route("/v1/users/password-forgot").post((req,res)=>{
 
 // 9.API to facebook login
 
- 
+// 10.API to search for a venue
+houndRoutes.route(search_venue).get((req,res)=>{
+    const venue_data = req.query;
+
+    const location = venue_data.location;
+    const type = venue_data.type;
+    const time = venue_data.time;
+    const date = venue_data.date;
+    const count = venue_data.count;
+    const minprice = venue_data.minprice;
+    const maxprice = venue_data.maxprice;
+
+    db.query("SELECT * FROM venbook WHERE location = '"+location+"' OR type = '"+type+"' OR time = '"+time+"' OR date = '"+date+"' OR count = '"+count+"' OR minprice = '"+minprice+"' OR maxprice = '"+maxprice+"';", (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+// 11.API to delete a venue
+houndRoutes.route(delete_venue).get((req,res)=>{
+    const venue_data = req.query;
+    const location = venue_data.location;
+    const type = venue_data.type;
+    const time = venue_data.time;
+    const date = venue_data.date;
+    const count = venue_data.count;
+    const minprice = venue_data.minprice;
+    const maxprice = venue_data.maxprice;
+
+    db.query("DELETE * FROM venbook WHERE location = '"+location+"' OR type = '"+type+"' OR time = '"+time+"' OR date = '"+date+"' OR count = '"+count+"' OR minprice = '"+minprice+"' OR maxprice = '"+maxprice+"';", (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+// 12.API to view a single venue
+houndRoutes.route(list_single_venue).get((req,res)=>{
+    const venue_data = req.query;
+    const location = venue_data.location;
+    const type = venue_data.type;
+    const time = venue_data.time;
+    const date = venue_data.date;
+    const count = venue_data.count;
+    const minprice = venue_data.minprice;
+    const maxprice = venue_data.maxprice;
+
+    db.query("SELECT * FROM venbook WHERE location = '"+location+"' OR type = '"+type+"' OR time = '"+time+"' OR date = '"+date+"' OR count = '"+count+"' OR minprice = '"+minprice+"' OR maxprice = '"+maxprice+"';", (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+// 13.API to book a venue
+houndRoutes.route(book_venue).get((req,res)=>{
+    const venue_data = req.query;
+    const location = venue_data.location;
+    const type = venue_data.type;
+    const time = venue_data.time;
+    const date = venue_data.date;
+    const count = venue_data.count;
+    const minprice = venue_data.minprice;
+    const maxprice = venue_data.maxprice;
+    
+    db.query("INSERT INTO venbook (location, type, time, date, count, minprice, maxprice) VALUES ('"+location+"', '"+type+"', '"+time+"', '"+date+"', '"+count+"', '"+minprice+"', '"+maxprice+"');", (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+  
 app.listen(_port,()=>{
     console.log("app running on port " + _port)
 })
