5b367d89028732a8f4717b95c87ee66c9ea4c89f
Arnie Mutasa <bantuhacker@gmail.com>
2021-12-20 16:17:25 +0200

    API BYO commit

--- index.js
+++ index.js
@@ -12,9 +12,33 @@ const { list_users } = require('./routes/index.js');
 const { delete_account } = require('./routes/index.js');
 const { single_user } = require('./routes/index.js');
 const { search_venue } = require('./routes/index.js');
+const { list_venues } = require('./routes/index.js');
 const { delete_venue } = require('./routes/index.js');
 const { list_single_venue } = require('./routes/index.js');
-const { book_venue } = require('./routes/index.js');
+
+
+// Venues
+const { book_venue, create_venue, create_venue_step_one, create_venue_step_two, create_venue_step_three, create_venue_step_four,
+    create_venue_step_five, create_venue_step_six, create_venue_step_seven, create_venue_step_eight, create_venue_step_nine } = require('./routes/index.js');
+
+// Menus
+const {list_venue_menus, list_venue_contracts } = require('./routes/index.js');
+
+
+// Venue Details
+const {list_venue_details, get_featured_places, get_last_minute_deals_places} = require('./routes/index.js');
+
+
+
+
+// Rooms
+
+const {get_room_details, get_room_pricing, update_room_pricing, add_favourite_place} = require('./routes/index.js');
+
+
+
+
+
 
 const { forgot_password } = require('./routes/index.js');
 
@@ -44,6 +68,7 @@ houndRoutes.route("/").get((req, res) => {
     res.json("Welcome to the eventhound server, contact admin for access");
     console.log(create_user);
 });*/
+
 
 // 1.API to create a new user
 houndRoutes.route(create_user).post((req,res)=>{
@@ -226,6 +251,319 @@ houndRoutes.route(book_venue).get((req,res)=>{
         res.send(result);
     })
 })
+
+
+// 14. API to create a new venue
+
+houndRoutes.route(create_venue).post((req,res)=>{
+
+    const { user_id, event_type, isFeatured, hasLastMinuteDeals, 
+        lastMinuteDealsDiscount,photoUrl, placeName, corporateName, 
+        inCorporateNumber, address, state, city, noticeRequired, autoConfirm, 
+        availableTiming, advanceDeposit, paymentStructure, guestDeposit, specialInstruction, 
+        accountNumber, transitNumber,InstitutionNumber, chequeName, chequeAddress, 
+        lat, lng, placeStatus, placeFeaturedStatus, placeSponsoredStatus, 
+        placeContract, placeContractPdf, dateCreated } = req.body
+    
+    let qr = "INSERT INTO event_places (user_id, event_type, isFeatured, hasLastMinuteDeals, lastMinuteDealsDiscount,photoUrl, placeName, corporateName, inCorporateNumber, address, state, city, noticeRequired, autoConfirm, availableTiming, advanceDeposit, paymentStructure, guestDeposit, special_instruction, accountNumber, transitNumber,InstitutionNumber, chequeName, chequeAddress, lat, lng, placeStatus, placeFeaturedStatus, placeSponsoredStatus, placeContract, placeContractPdf, dateCreated) VALUES ('"+user_id+"', '"+event_type+"', '"+isFeatured+"', '"+hasLastMinuteDeals+"', '"+lastMinuteDealsDiscount+"','"+photoUrl+"', '"+placeName+"', '"+corporateName+"', '"+inCorporateNumber+"', '"+address+"', '"+state+"', '"+city+"', '"+noticeRequired+"', '"+autoConfirm+"', '"+availableTiming+"', '"+advanceDeposit+"', '"+paymentStructure+"', '"+guestDeposit+"', '"+specialInstruction+"', '"+accountNumber+"', '"+transitNumber+"','"+InstitutionNumber+"', '"+chequeName+"', '"+chequeAddress+"', '"+lat+"', '"+lng+"', '"+placeStatus+"', '"+placeFeaturedStatus+"', '"+placeSponsoredStatus+"', '"+placeContract+"', '"+placeContractPdf+"', '"+dateCreated+"');"
+    
+    db.query(qr, (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+
+// 14. API to step by step update venue
+houndRoutes.route(create_venue_step_one).post((req,res)=>{
+
+    const { email, contact_person, contact_number, corporate_name, in_corporate_number,mail_frequency, place_name, address, city, state, status, token, user_id, autoconfirm, notice_required, open_time, close_time } = req.body
+    
+    let qr = "INSERT INTO venue (email, contact_person, contact_number, corporate_name, in_corporate_number,mail_frequency, place_name, address, city, state, status, token, user_id, autoconfirm, notice_required, open_time, close_time) VALUES ('"+email+"', '"+contact_person+"', '"+contact_number+"', '"+corporate_name+"', '"+in_corporate_number+"','"+mail_frequency+"', '"+place_name+"', '"+address+"', '"+city+"', '"+state+"', '"+status+"', '"+token+"', '"+user_id+"', '"+autoconfirm+"', '"+notice_required+"', '"+open_time+"', '"+close_time+"');"
+    
+    db.query(qr, (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+// 15. API create venue step two
+houndRoutes.route(create_venue_step_two).put((req,res)=>{
+
+    const { autoConfirm, noticeRequired, place_id } = req.body
+    
+    let qr = "UPDATE venue SET autoconfirm = '"+autoConfirm+"', notice_required = '"+noticeRequired+"' WHERE venue_id = '"+place_id+"';"
+    
+    db.query(qr, (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+
+// 16. API create venue step three
+houndRoutes.route(create_venue_step_three).post((req,res)=>{
+
+    const { openTime, closeTime, place_id } = req.body
+    
+    let qr = "INSERT INTO venue_hours (open_time, close_time, place_id) VALUES ('"+openTime+"','"+closeTime+"','"+place_id+"');"
+    
+    db.query(qr, (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+// 17. API create venue step four // TODO - iterate through multiple files and upload
+houndRoutes.route(create_venue_step_four).post((req,res)=>{
+    let files = []
+
+    if (typeof req.body.files === "string") {
+        files.push(req.body.files);
+    } else {
+        files = req.body.files;
+    }
+
+    const { token, place_id, menu_name } = req.body
+    
+    let qr = "INSERT INTO venue_menus (token, place_id, menu_name) VALUES ('"+token+"','"+place_id+"','"+menu_name+"');"
+    
+    db.query(qr, (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+
+// 18. API create venue step five
+houndRoutes.route(create_venue_step_five).post((req,res)=>{
+
+    const { advanceDeposit, guestDeposit, place_id, special_instruction, transitNumber, institutionNumber, accountNumber, chequeName, chequeAddress } = req.body
+    
+    let qr = "INSERT INTO venue_deposits (advance_deposit, guest_deposit, place_id,special_instruction, transit_number, institution_number, account_number, cheque_name, cheque_address) VALUES ('"+advanceDeposit+"','"+guestDeposit+"','"+place_id+"','"+special_instruction+"','"+transitNumber+"','"+institutionNumber+"','"+accountNumber+"','"+chequeName+"','"+chequeAddress+"');"
+    
+    db.query(qr, (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+
+// 19. API create venue step six --- CREATE ROOM
+houndRoutes.route(create_venue_step_six).post((req,res)=>{
+
+    const { room_name, maximum_guests, maximum_guests_seated, maximum_guests_seated_with_dance_floor, room_description, place_id } = req.body
+    
+    let qr = "INSERT INTO rooms (room_name, maximum_guests, maximum_guests_seated, maximum_guests_seated_with_dance_floor, room_description, place_id) VALUES ('"+room_name+"','"+maximum_guests+"','"+maximum_guests_seated+"','"+maximum_guests_seated_with_dance_floor+"','"+room_description+"','"+place_id+"');"
+    
+    db.query(qr, (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+// 20. API create venue step seven
+houndRoutes.route(create_venue_step_seven).post((req,res)=>{
+
+    const { } = req.body
+    
+    let qr = ""
+    
+    db.query(qr, (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+// 21. API create venue step eight - PRICING
+houndRoutes.route(create_venue_step_eight).post((req,res)=>{
+
+    const { startDate, endDate, room_id, dayPrice, nightPrice, fullDayPrice,minSpendDay,minSpendNight, minSpendFullDay} = req.body
+    
+    let qr = "INSERT INTO room_pricing (startDate, endDate, room_id, dayPrice, nightPrice, fullDayPrice,minSpendDay,minSpendNight, minSpendFullDay) VALUES ('"+startDate+"','"+endDate+"','"+room_id+"','"+dayPrice+"','"+nightPrice+"','"+fullDayPrice+"','"+minSpendDay+"','"+minSpendNight+"','"+minSpendFullDay+"');"
+    
+    db.query(qr, (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+
+
+
+// 22.API to list all venues
+houndRoutes.route(list_venues).get((req,res)=>{
+    const venue_data = req.query;
+    const place_id = venue_data.place_id;
+    const place_name = venue_data.placeName;
+    const corporate_name = venue_data.corporateName;
+
+    db.query('SELECT * FROM event_places;',(err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+
+// 23. API get list of menus for a venue
+// Route : /auth/getMenuList'
+houndRoutes.route(list_venue_menus).get((req,res)=>{
+
+    const { place_id } = req.query
+    
+    let qr = "SELECT * FROM venue_menus WHERE place_id = '"+place_id+"'";
+    
+    db.query(qr, (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+
+// 24. API get contracts for a venue
+// Route : /auth/getContractList'
+houndRoutes.route(list_venue_contracts).get((req,res)=>{
+
+    const { place_id } = req.query
+    
+    let qr = "SELECT * FROM venue_contracts WHERE place_id = '"+place_id+"'";
+    
+    db.query(qr, (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+
+// 25. API get venue details
+// Route : /auth/getVenueDetail
+houndRoutes.route(list_venue_details).get((req,res)=>{
+
+    const { place_id } = req.query
+
+
+    console.log(place_id)
+    
+    let qr = "SELECT * FROM venue WHERE venue_id = '"+place_id+"'";
+    
+    db.query(qr, (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+
+// 26. API get featured places
+// Route : /api/getFeaturePlaces
+houndRoutes.route(get_featured_places).get((req,res)=>{
+
+        
+    let qr = "SELECT * FROM venue INNER JOIN featured_venues ON venue.venue_id = featured_venues.place_id WHERE featured_venues.featured = 1";
+    
+    db.query(qr, (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+// 26. API get last minute deals
+// Route : /api/getLastMinuteDealPlaces
+houndRoutes.route(get_last_minute_deals_places).get((req,res)=>{
+
+        
+    let qr = "SELECT * FROM last_minute_deals INNER JOIN venue ON venue.venue_id = last_minute_deals.place_id";
+    
+    db.query(qr, (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+
+// 27. API get room details
+// Route : /api/getRoomDetails
+houndRoutes.route(get_room_details).get((req,res)=>{
+
+    const room_id = req.query.room_id
+        
+    let qr = "SELECT * FROM rooms where room_id = '"+room_id+"'";
+    
+    db.query(qr, (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+// 28. API get room pricing
+// Route : /auth/getRoomPricing
+houndRoutes.route(get_room_pricing).get((req,res)=>{
+
+    const room_id = req.query.room_id
+        
+    let qr = "SELECT * FROM room_pricing where room_id = '"+room_id+"'";
+    
+    db.query(qr, (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+
+// 29. API add favourite place
+// Route : /auth/addFavourite
+houndRoutes.route(add_favourite_place).post((req,res)=>{
+
+    const {room_id, user_id} = req.body
+        
+    let qr = "INSERT INTO favourite_places (user_id, room_id) VALUES ('"+user_id+"','"+room_id+"')";
+    
+    db.query(qr, (err,result)=>{
+        if(err){
+        res.json(err);
+        }
+        res.send(result);
+    })
+})
+
+
+
+
+
+
   
 app.listen(_port,()=>{
     console.log("app running on port " + _port)
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,36 @@ exports.google_login = '/users/login/google';
 exports.facebook_login = '/users/login/facebook';
 
 exports.create_venue = '/venue/create';
+exports.create_venue_step_one = '/api/firstStep';
+exports.create_venue_step_two = '/auth/secondStep';
+exports.create_venue_step_three = '/auth/thirdStep';
+exports.create_venue_step_four = '/auth/fourthStep';
+exports.create_venue_step_five = '/auth/fifthStep';
+exports.create_venue_step_six = '/auth/sixthStep';
+exports.create_venue_step_seven = '/auth/seventhStep';
+exports.create_venue_step_eight = '/auth/eightStep';
+exports.create_venue_step_nine = '/venue/create-step-nine';
+
+exports.list_venue_menus = '/auth/getMenuList'
+exports.list_venue_contracts = '/auth/getContractList'
+exports.list_venue_details = '/auth/getVenueDetail'
+
+
+
+exports.get_featured_places = '/api/getFeaturePlaces'
+exports.get_last_minute_deals_places = '/api/getLastMinuteDealPlaces'
+
+
+
+exports.get_room_details = '/api/getRoomDetails'
+exports.get_room_pricing = '/auth/getRoomPricing'
+exports.update_room_pricing = '/auth/updateRoomPricing'
+exports.add_favourite_place = '/auth/addFavourite'
+
+
+
+
+
 exports.edit_venue = '/venue/edit';
 exports.list_venues = '/venue/list';
 exports.list_single_venue = '/venue/single';
